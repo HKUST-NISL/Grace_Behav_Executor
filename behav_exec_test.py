a77ae1c18b4004b188765643494ffba10a4d7645
TheDoctor-JI <yifanchangle@163.com>
2023-07-12 15:40:40 +0800

    Updated test script configs

--- behav_exec_test.py
+++ behav_exec_test.py
@@ -18,21 +18,23 @@ import std_msgs
 
 
 #Load configs
-def loadConfigs():
-    #Load configs
-    with open("./config/config.yaml", "r") as config_file:
-        configs = yaml.load(config_file, Loader=yaml.FullLoader)
-        print("Read successful")
-    return configs
+import sys
+import os
+from inspect import getsourcefile
 
-configs = loadConfigs()
 
 
 if __name__ == '__main__':
+
+    file_path = os.path.dirname(os.path.realpath(getsourcefile(lambda:0)))
+    sys.path.append(os.path.join(file_path, '..'))
+    from CommonConfigs.grace_cfg_loader import *
+    configs = loadGraceConfigs()
+
     #Ros routine
     rospy.init_node("exec_test")
     grace_behavior_client = rospy.ServiceProxy(
-        configs['Ros']['grace_behavior_service'], 
+        configs['Custom']['Behavior']['grace_behavior_service'], 
         grace_attn_msgs.srv.GraceBehavior)
 
 
@@ -41,8 +43,8 @@ if __name__ == '__main__':
 
     # #(1) Test Following, Aversion
     # req = grace_attn_msgs.srv.GraceBehaviorRequest()
-    # # req.command = configs['General']['head_gaze_avert']
-    # req.command = configs['General']['head_gaze_follow']
+    # req.command = configs['BehavExec']['General']['head_gaze_avert']
+    # # req.command = configs['BehavExec']['General']['head_gaze_follow']
     # print("Service call response is:\n %s" % grace_behavior_client(req))
     
 
@@ -50,30 +52,30 @@ if __name__ == '__main__':
 
     # #(2) Test Nodding
     # req = grace_attn_msgs.srv.GraceBehaviorRequest()
-    # req.command = configs['General']['nod_cmd']
+    # req.command = configs['BehavExec']['General']['nod_cmd']
     # print("Service call response is:\n %s" % grace_behavior_client(req))
 
 
 
 
-    #(3) Test Composite
-    req = grace_attn_msgs.srv.GraceBehaviorRequest()
-    req.command = configs['General']['comp_behav_exec_cmd']
-    # req.command = configs['General']['hum_behav_exec_cmd']
-    req.utterance = configs['CompositeBehavior']['Predefined']['Sample']['txt']
-    req.lang = configs['CompositeBehavior']['Predefined']['Sample']['lang']
+    # #(3) Test Composite
+    # req = grace_attn_msgs.srv.GraceBehaviorRequest()
+    # req.command = configs['BehavExec']['General']['comp_behav_exec_cmd']
+    # # req.command = configs['BehavExec']['General']['hum_behav_exec_cmd']
+    # req.utterance = configs['BehavExec']['CompositeBehavior']['Predefined']['Sample']['txt']
+    # req.lang = configs['BehavExec']['CompositeBehavior']['Predefined']['Sample']['lang']
 
-    req.expressions = configs['CompositeBehavior']['Predefined']['Sample']['expressions']
-    req.exp_start = configs['CompositeBehavior']['Predefined']['Sample']['exp_start']
-    req.exp_end = configs['CompositeBehavior']['Predefined']['Sample']['exp_end']
-    req.exp_mag = configs['CompositeBehavior']['Predefined']['Sample']['exp_mag']
+    # req.expressions = configs['BehavExec']['CompositeBehavior']['Predefined']['Sample']['expressions']
+    # req.exp_start = configs['BehavExec']['CompositeBehavior']['Predefined']['Sample']['exp_start']
+    # req.exp_end = configs['BehavExec']['CompositeBehavior']['Predefined']['Sample']['exp_end']
+    # req.exp_mag = configs['BehavExec']['CompositeBehavior']['Predefined']['Sample']['exp_mag']
 
-    req.gestures = configs['CompositeBehavior']['Predefined']['Sample']['gestures']
-    req.ges_start = configs['CompositeBehavior']['Predefined']['Sample']['ges_start']
-    req.ges_end = configs['CompositeBehavior']['Predefined']['Sample']['ges_end']
-    req.ges_mag = configs['CompositeBehavior']['Predefined']['Sample']['ges_mag']
+    # req.gestures = configs['BehavExec']['CompositeBehavior']['Predefined']['Sample']['gestures']
+    # req.ges_start = configs['BehavExec']['CompositeBehavior']['Predefined']['Sample']['ges_start']
+    # req.ges_end = configs['BehavExec']['CompositeBehavior']['Predefined']['Sample']['ges_end']
+    # req.ges_mag = configs['BehavExec']['CompositeBehavior']['Predefined']['Sample']['ges_mag']
 
-    print("Service call response is:\n %s" % grace_behavior_client(req))
+    # print("Service call response is:\n %s" % grace_behavior_client(req))
 
 
 
